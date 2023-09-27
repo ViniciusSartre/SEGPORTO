6a165131af115d61b561ea3c923407758f632235
Vinicius Sartre <vinicius_sartre@hotmail.com>
2023-09-27 15:19:34 -0300

    updating archives

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,29 @@ openMenuButton.addEventListener('click', function () {
     menu.style.display = 'block';
   }
 });
+
+function acessarCamera() {
+  var captureInput = document.getElementById('captureInput');
+  if (captureInput) {
+    captureInput.click();
+    captureInput.addEventListener('change', function (event) {
+      var imageFile = event.target.files[0];
+      if (imageFile) {
+        // Agora você pode fazer algo com a imagem, como exibi-la na página ou enviá-la para um servidor.
+        var imageElement = document.createElement('img');
+        imageElement.src = URL.createObjectURL(imageFile);
+        document.body.appendChild(imageElement);
+      }
+    });
+  }
+}
+
+	// Adicione um ouvinte de evento para o botão "Contatos de Emergência"
+  document.getElementById('mostrarContatosBtn').addEventListener('click', function () {
+    var listaContatos = document.getElementById('lista-contatos');
+    if (listaContatos.style.display === 'none' || listaContatos.style.display === '') {
+      listaContatos.style.display = 'block';
+    } else {
+      listaContatos.style.display = 'none';
+    }
+  });
